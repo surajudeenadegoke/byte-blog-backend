8af819a432f88803421db503186c77f898687114
Decagon <decagon@Suraj-MacBook.local>
2025-09-19 23:46:54 +0100

    setup Express server and MongoDB connection

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+        console.error("MONGO_URI is not defined in environment variables");
+        process.exit(1); // Exit process with failure
+    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
+connectDB();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+// Sample route to test the server
+app.get("/", (req, res) => {
+  console.log("my message");
+  res.send("Welcome to the ByteBlog API.Server is running smoothly.");});
+
+//404 route
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+//Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Minimal server running on http://localhost:${PORT}`);
+});
